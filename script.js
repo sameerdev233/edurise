26d660505d49749bbc27169307bfd3b9bc036480
sameerdev233 <sk1723175@gmail.com>
2026-07-10 17:45:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Welcome to Edurise!");
+function login() {
+  let name = document.getElementById("name").value;
+
+  if (name == "") {
+    alert("Pehle apna naam likho");
+    return;
+  }
+
+  localStorage.setItem("studentName", name);
+
+  alert("Welcome " + name);
+}
